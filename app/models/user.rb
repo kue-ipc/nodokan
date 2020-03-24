fc79a592693739dbf566cb10225424179f3baef6
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-03-24 14:48:27 +0900

    userについてログインアカウントをusernameにする。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,32 @@
 class User < ApplicationRecord
   rolify
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :ldap_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :registerable, :recoverable, :rememberable, :validatable
+  devise :ldap_authenticatable
+
+  def ldap_before_save
+    sync_ldap!
+  end
+
+  def ldap_entry
+    @ldap_entry ||= Devise::LDAP::Adapter.get_ldap_entry(username)
+  end
+
+  def ldap_mail
+    ldap_entry&.[]('mail')&.first
+  end
+
+  def ldap_display_name
+    ldap_entry&.[]("displayName;lang-#{I18n.default_locale}")&.first ||
+      ldap_entry&.[]('displayName')&.first
+  end
+
+  def sync_ldap!
+    return unless ldap_entry
+
+    self.email = ldap_mail
+    self.fullname = ldap_display_name
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,15 +26,13 @@ ActiveRecord::Schema.define(version: 2020_03_24_025000) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "fullname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
