06762fbf76a3cd9f1171a3b90468485d28f942eb
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-04-01 17:02:21 +0900

    その他設定中

--- app/models/hardware.rb
+++ app/models/hardware.rb
@@ -1,3 +1,14 @@
 class Hardware < ApplicationRecord
   has_many :node, dependent: :restrict_with_error
+
+  enum category: {
+    desktop: 0,
+    laptop: 1,
+    tablet: 2,
+    mobile: 3,
+    server: 4,
+    network: 5,
+    virtual: 6,
+    other: 255
+  }
 end
--- app/models/operating_system.rb
+++ app/models/operating_system.rb
@@ -1,3 +1,15 @@
 class OperatingSystem < ApplicationRecord
   has_many :node, dependent: :restrict_with_error
+
+  enum category: {
+    windows: 0,
+    mac: 1,
+    ios: 2,
+    android: 3,
+    linux: 4,
+    bsd: 5,
+    unix: 6,
+    dedicated: 7,
+    other: 255
+  }
 end
