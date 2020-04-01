d3e3ed6cb717e30a6de43dc7d26ec1fcb450ad17
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-04-01 14:17:48 +0900

    reactではなくcofeescript

--- babel.config.js
+++ babel.config.js
@@ -22,8 +22,10 @@ module.exports = function(api) {
         {
           targets: {
             node: 'current'
-          }
-        }
+          },
+          modules: 'commonjs'
+        },
+        '@babel/preset-react'
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         '@babel/preset-env',
@@ -33,6 +35,13 @@ module.exports = function(api) {
           corejs: 3,
           modules: false,
           exclude: ['transform-typeof-symbol']
+        }
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: isDevelopmentEnv || isTestEnv,
+          useBuiltIns: true
         }
       ]
     ].filter(Boolean),
@@ -65,6 +74,13 @@ module.exports = function(api) {
         '@babel/plugin-transform-regenerator',
         {
           async: false
+        }
+      ],
+      [
+        "@babel/plugin-transform-react-jsx",
+        {
+          pragma: 'h', // for Hyperapp
+          pragmaFrag: '""' // for Hyperapp
         }
       ]
     ].filter(Boolean)
