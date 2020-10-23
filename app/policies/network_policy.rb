cd96b088b765dda7dad004887fffed7221cd1507
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-10-23 17:02:47 +0900

    次はネットワーク

--- app/policies/network_policy.rb
+++ app/policies/network_policy.rb
@@ -1,7 +1,15 @@
 class NetworkPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      if @user.admin?
+        scope.all
+      else
+        scope.where(users: @user)
+      end
     end
+  end
+
+  def index?
+    true
   end
 end
