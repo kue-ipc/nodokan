477cb75ab88b7f65af2b998a8ba00a7d21176384
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-10-27 13:29:56 +0900

    フォーマットの変更のみ

--- lib/devise_ldap_authenticatable_nis_group_check.rb
+++ lib/devise_ldap_authenticatable_nis_group_check.rb
@@ -29,25 +29,31 @@ module Devise
       def in_group_nis?(group_name)
         in_group = false
 
-        group_checking_ldap = if @check_group_membership_without_admin
-                                @ldap
-                              else
-                                Connection.admin
-                              end
+        group_checking_ldap =
+          if @check_group_membership_without_admin
+            @ldap
+          else
+            Connection.admin
+          end
 
-        group_checking_ldap.search(base: group_name,
-                                   scope: Net::LDAP::SearchScope_BaseObject) do |entry|
+        group_checking_ldap.search(
+          base: group_name,
+          scope: Net::LDAP::SearchScope_BaseObject
+        ) do |entry|
           if entry[LDAP::DEFAULT_GID_NUMBER_KEY][0].to_i == gid_number
             in_group = true
-            DeviseLdapAuthenticatable::Logger.send("User #{dn} is included in nis primary group: #{group_name}")
+            DeviseLdapAuthenticatable::Logger.send(
+              "User #{dn} is included in nis primary group: #{group_name}")
           elsif entry[LDAP::DEFAULT_MEMBER_UID_KEY].include?(uid)
             in_group = true
-            DeviseLdapAuthenticatable::Logger.send("User #{dn} is included in nis group: #{group_name}")
+            DeviseLdapAuthenticatable::Logger.send(
+              "User #{dn} is included in nis group: #{group_name}")
           end
         end
 
         unless in_group
-          DeviseLdapAuthenticatable::Logger.send("User #{dn} is not in nis group: #{group_name}")
+          DeviseLdapAuthenticatable::Logger.send(
+            "User #{dn} is not in nis group: #{group_name}")
         end
 
         in_group
@@ -55,8 +61,10 @@ module Devise
 
       # overwrite in_group?
       alias _in_group? in_group?
-      def in_group?(group_name,
-                    group_attribute = LDAP::DEFAULT_GROUP_UNIQUE_MEMBER_LIST_KEY)
+      def in_group?(
+        group_name,
+        group_attribute = LDAP::DEFAULT_GROUP_UNIQUE_MEMBER_LIST_KEY
+      )
         if ::Devise.ldap_nis_group_check
           in_group_nis?(group_name)
         else
