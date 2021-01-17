dc8d59ff723b8d777299c7abcebac70ac4bdb383
raccy <open@raccy.org>
2021-01-17 11:11:59 +0900

    cssもwebpackで

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const coffee =  require('./loaders/coffee')
 
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+});
+
 environment.loaders.prepend('coffee', coffee)
 module.exports = environment
