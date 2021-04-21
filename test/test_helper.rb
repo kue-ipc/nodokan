6a97864fd977e0220fbfb38be1ce7728aeaa6e18
raccy <open@raccy.org>
2021-04-21 22:50:19 +0900

    fitureを修正中

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,29 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'base64'
+
+module FixtureAddressHelper
+  def ipv4_address(address)
+    '!!binary "' +
+    Base64.strict_encode64(IPAddress::IPv4.new(address).data) +
+    '"'
+  end
+
+  def ipv6_address(address)
+    '!!binary "' +
+    Base64.strict_encode64(IPAddress::IPv6.new(address).data) +
+    '"'
+  end
+
+  def mac_address(address)
+    '!!binary "' +
+    Base64.strict_encode64([address.delete('-:')].pack('H12')) +
+    '"'
+  end
+end
+
+ActiveRecord::FixtureSet.context_class.include FixtureAddressHelper
 
 module ActiveSupport
   class TestCase
