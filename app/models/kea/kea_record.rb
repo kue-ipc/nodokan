4d70944c8555f353f2a18258b09e0a95b11f8aa5
KUE IPC <ipc@kyokyo-u.ac.jp>
2021-05-21 16:38:51 +0900

    auditの設定が取りあえずできた。

--- app/models/kea/kea_record.rb
+++ app/models/kea/kea_record.rb
@@ -7,7 +7,15 @@ module Kea
     before_destroy :set_disable_audit
 
     def set_disable_audit
-      self.class.connection.execute('SET @disable_audit = 1;')
+      # self.class.connection.execute('SET @disable_audit = 1;')
+      self.class.connection.execute <<-SQL
+        INSERT INTO dhcp4_audit_revision
+        (modification_ts, server_id, log_message)
+        VALUES (NOW(), 1, '');
+      SQL
+      self.class.connection.execute('SET @audit_revision_id = LAST_INSERT_ID();')
+      self.class.connection.execute('SET @cascade_transaction = 0;')
+      # SET @audit_revision_id = LAST_INSERT_ID();
     end
   end
   # rubocop:enable Rails/ApplicationRecord
