793ed495a967b44a5967797570f5206176f7ccae
KUE IPC <ipc@kyokyo-u.ac.jp>
2021-06-08 13:18:34 +0900

    syncを追加

---
+++ app/jobs/kea_subnet_check_all_job.rb
@@ -0,0 +1,14 @@
+class KeaSubnetCheckAllJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    network4_ids = Network.where(dhcp: true).where.not(ipv4_network_data: nil).ids
+    Kea::Dhcp4Subnet.transaction do
+      Kea::Dhcp4Subnet.dhcp4_audit
+      Kea::Dhcp4Subnet.where.not(subnet_id: network4_ids).destroy_all
+    end
+
+    # TODO
+    # dhcp6
+  end
+end
