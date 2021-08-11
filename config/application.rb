effdac6ba45e32b599b550feaa9d2b280982e9ca
KUE IPC <ipc@kyokyo-u.ac.jp>
2021-08-11 10:05:56 +0900

    v0.1.0

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Nodokan
-  VERSION = '0.0.6'.freeze
+  VERSION = '0.1.0'.freeze
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
