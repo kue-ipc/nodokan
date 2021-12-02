775906a3ba11c705e96566c8e458a1e9250c9d01
KUE IPC <ipc@kyokyo-u.ac.jp>
2021-12-02 15:34:11 +0900

    古いものなどの整理

--- config/webpack_old/environment.js
+++
@@ -1,9 +0,0 @@
-const { environment } = require('@rails/webpacker')
-const coffee =  require('./loaders/coffee')
-
-environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader'
-});
-
-environment.loaders.prepend('coffee', coffee)
-module.exports = environment
