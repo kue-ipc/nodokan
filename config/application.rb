676890731435e0db972a1fd11b1ebe79f48007aa
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-12-21 15:28:29 +0900

    v0.5.9

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module Nodokan
-  VERSION = "0.5.8".freeze
+  VERSION = "0.5.9".freeze
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
