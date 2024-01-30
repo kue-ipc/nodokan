bd1f397d2d44a35aa8720813d4471c2e38ef7022
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-01-30 11:46:55 +0900

    db:migrateによりスキームの更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_20_062456) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_062456) do
   create_table "assignments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "network_id", null: false
