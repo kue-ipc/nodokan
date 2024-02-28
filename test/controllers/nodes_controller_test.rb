7a8bebfd9185f5ab5310d4d54d2b3cf5fd271402
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-02-28 16:59:13 +0900

    色々追加

--- test/controllers/nodes_controller_test.rb
+++ test/controllers/nodes_controller_test.rb
@@ -92,25 +92,85 @@ class NodesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "user should get edit" do
+    sign_in users(:user)
+    get edit_node_url(@node)
+    assert_response :success
+  end
+
+  test "other should NOT get edit" do
+    sign_in users(:other)
+    get edit_node_url(@node)
+    assert_response :not_found
+  end
+
+  test "redirect to login INSTEAD OF get edit" do
+    get edit_node_url(@node)
+    assert_redirected_to new_user_session_path
+  end
+
   # create
 
   test "admin should create node" do
     sign_in users(:admin)
     assert_difference("Node.count") do
-      post nodes_url,
-        params: {node: {name: @node.name, note: @node.note, user_id: users(:admin).id}}
+      post nodes_url, params: {node: {name: @node.name, note: @node.note, user_id: @node.user_id}}
     end
 
     assert_redirected_to node_url(Node.last)
+    assert_equal users(:user).id, Node.last.user_id
+  end
+
+  test "user should create node" do
+    sign_in users(:user)
+    assert_difference("Node.count") do
+      post nodes_url, params: {node: {name: @node.name, note: @node.note, user_id: @node.user_id}}
+    end
+
+    assert_redirected_to node_url(Node.last)
+    assert_equal users(:user).id, Node.last.user_id
+  end
+
+  test "other should create node" do
+    sign_in users(:other)
+    assert_difference("Node.count") do
+      post nodes_url, params: {node: {name: @node.name, note: @node.note, user_id: @node.user_id}}
+    end
+
+    assert_redirected_to node_url(Node.last)
+    assert_equal users(:other).id, Node.last.user_id
+  end
+
+  test "redirect to login INSTEAD OF create node" do
+    assert_no_difference("Node.count") do
+      post nodes_url, params: {node: {name: @node.name, note: @node.note, user_id: @node.user_id}}
+    end
+    assert_redirected_to new_user_session_path
   end
 
   # update
 
   test "admin should update node" do
     sign_in users(:admin)
-    patch node_url(@node),
-      params: {node: {name: @node.name, note: @node.note}}
+    patch node_url(@node), params: {node: {name: @node.name, note: @node.note}}
     assert_redirected_to node_url(@node)
+  end
+
+  test "user should update node" do
+    sign_in users(:user)
+    patch node_url(@node), params: {node: {name: @node.name, note: @node.note}}
+    assert_redirected_to node_url(@node)
+  end
+
+  test "other should NOT update node" do
+    sign_in users(:other)
+    patch node_url(@node), params: {node: {name: @node.name, note: @node.note}}
+    assert_response :not_found
+  end
+
+  test "redirect to login INSTEAD OF update node" do
+    patch node_url(@node), params: {node: {name: @node.name, note: @node.note}}
+    assert_redirected_to new_user_session_path
   end
 
   # destroy
@@ -120,8 +180,30 @@ class NodesControllerTest < ActionDispatch::IntegrationTest
     assert_difference("Node.count", -1) do
       delete node_url(@node)
     end
-
     assert_redirected_to nodes_url
+  end
+
+  test "user should destroy node" do
+    sign_in users(:user)
+    assert_difference("Node.count", -1) do
+      delete node_url(@node)
+    end
+    assert_redirected_to nodes_url
+  end
+
+  test "other should destroy node" do
+    sign_in users(:other)
+    assert_no_difference("Node.count") do
+      delete node_url(@node)
+    end
+    assert_response :not_found
+  end
+
+  test "redirect to login INSTEAD OF destroy node" do
+    assert_no_difference("Node.count") do
+      delete node_url(@node)
+    end
+    assert_redirected_to new_user_session_path
   end
 
   # copy
