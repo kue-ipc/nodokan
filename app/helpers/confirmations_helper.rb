69f12f7abbd0913a2f8cae7730ad0d91a0ebaeb5
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-04-10 10:45:57 +0900

    formをすべて書き換え

--- app/helpers/confirmations_helper.rb
+++ app/helpers/confirmations_helper.rb
@@ -1,19 +1,29 @@
 module ConfirmationsHelper
-  def confirmation_question_radio_buttons(form, name, count: nil,
+  # for enum
+  def confirmation_question_radio_buttons(form, name, count: nil, excludes: [],
     required: false)
-    confirmation_question_group(form, name, count: nil) do
-      t_enums(name.to_s.pluralize.intern, Confirmation).map { |key, message|
-        form.radio_button(name, key, label: message, required: required,
-          data: {node_confirmation_target: name})
-      }.inject(:+)
-    end
+    pluralized_name = name.to_s.pluralize.intern
+    list = t_enums(pluralized_name, Confirmation).except(*excludes).to_a
+    form.collection_radio_buttons(name, list, :first, :second,
+      label: {text: confirmation_question_label(name, count: count)},
+      help: raw(t(name, scope: "messages.node_confirm_helps", default: nil)),
+      required: required, data: {node_confirmation_target: name})
+  end
+
+  # for bitwise
+  def confirmation_question_check_boxes(form, name, count: nil, excludes: [],
+    required: false)
+    pluralized_name = name.to_s.pluralize.intern
+    list = t_bitwises(pluralized_name, Confirmation).except(*excludes).to_a
+    form.collection_check_boxes(pluralized_name, list, :first, :second,
+      label: {text: confirmation_question_label(name, count: count)},
+      help: raw(t(name, scope: "messages.node_confirm_helps", default: nil)),
+      required: required, data: {node_confirmation_target: name})
   end
 
   def confirmation_question_group(form, name, count: nil, &block)
     form.form_group(name,
-      label: {
-        text: confirmation_question_label(name, count: count),
-      },
+      label: {text: confirmation_question_label(name, count: count)},
       help: raw(t(name, scope: "messages.node_confirm_helps", default: nil)),
       &block)
   end
