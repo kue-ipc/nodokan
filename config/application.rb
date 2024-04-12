029826a50f112aad1f4a23af53bda807e9e6db66
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-04-12 13:35:51 +0900

    v0.6.2

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module Nodokan
-  VERSION = -"0.6.1"
+  VERSION = -"0.6.2"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
