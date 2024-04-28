31553634d8e789e4ce38744c87fc49aac4440a8b
raccy <raccy@users.noreply.github.com>
2024-04-28 12:23:32 +0900

    ユーザー同期jobもテストできるようにする

--- test/jobs/users_sync_job_test.rb
+++ test/jobs/users_sync_job_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class UsersSyncJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @adapter = Minitest::Mock.new
+    @adapter.expect(:get_login_list, users.map(&:username))
+    User.where(deleted: false).find_each do |user|
+      unless user.deleted
+        @adapter.expect(:authorizable?, true, [user.username])
+        entry = Net::LDAP::Entry.new
+        entry["mail"] = user.email
+        entry["displayName"] = user.fullname
+        @adapter.expect(:get_ldap_entry, entry, [user.username])
+      end
+    end
+  end
+
+  test "user sync" do
+    Devise::LDAP.stub_const :Adapter, @adapter do
+      perform_enqueued_jobs do
+        UsersSyncJob.perform_later
+      end
+    end
+    @adapter.verify
+  end
 end
