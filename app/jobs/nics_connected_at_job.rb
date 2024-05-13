1cc8328f31c26f6995ef8dd8096de4ba3e3956f3
KUE IPC <ipc@kyokyo-u.ac.jp>
2024-05-13 16:57:52 +0900

    全てについて接続日時が出るようになる

--- app/jobs/nics_connected_at_job.rb
+++ app/jobs/nics_connected_at_job.rb
@@ -15,21 +15,21 @@ class NicsConnectedAtJob < ApplicationJob
   end
 
   def ipv4_resolved_at(nic)
-    if nic.has_ipv4?
-      Ipv4Arp.where(ipv4_data: nic.ipv4_data)
-        .order(:resolved_at).last&.resolved_at
-    elsif nic.has_mac_address?
+    if nic.has_mac_address?
       Ipv4Arp.where(mac_address_data: nic.mac_address_data)
+        .order(:resolved_at).last&.resolved_at
+    elsif nic.has_ipv4?
+      Ipv4Arp.where(ipv4_data: nic.ipv4_data)
         .order(:resolved_at).last&.resolved_at
     end
   end
 
   def ipv6_discovered_at(nic)
-    if nic.has_ipv6?
-      Ipv6Neighbor.where(ipv6_data: nic.ipv6_data)
-        .order(:discovered_at).last&.discovered_at
-    elsif nic.has_mac_address?
+    if nic.has_mac_address?
       Ipv6Neighbor.where(mac_address_data: nic.mac_address_data)
+        .order(:discovered_at).last&.discovered_at
+    elsif nic.has_ipv6?
+      Ipv6Neighbor.where(ipv6_data: nic.ipv6_data)
         .order(:discovered_at).last&.discovered_at
     end
   end
@@ -43,10 +43,6 @@ class NicsConnectedAtJob < ApplicationJob
 
   def ipv6_leased_at(nic)
     return unless nic.node.has_duid? && nic.network_id
-
-    warn "hogehoge"
-    warn(Kea::Lease6.where(duid: nic.node.duid_data, subnet_id: nic.network.id)
-    .order(:expire).last&.leased_at)
 
     Kea::Lease6.where(duid: nic.node.duid_data, subnet_id: nic.network.id)
       .order(:expire).last&.leased_at
