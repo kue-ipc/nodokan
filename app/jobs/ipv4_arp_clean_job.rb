8abcb3a90a0dbf1b0fb11a65bec6eacce973870c
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-05-15 09:37:49 +0900

    TODOを付ける

--- app/jobs/ipv4_arp_clean_job.rb
+++ app/jobs/ipv4_arp_clean_job.rb
@@ -2,6 +2,7 @@ class Ipv4ArpCleanJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    # TODO: 実装
     # Do something later
   end
 end
--- app/jobs/radius_clean_job.rb
+++ app/jobs/radius_clean_job.rb
@@ -2,6 +2,7 @@ class RadiusCleanJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    # TODO: 実装
     # Do something later
   end
 end
