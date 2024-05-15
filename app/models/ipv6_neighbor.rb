0394819e427e8cd91ae83762fc9d7f6df3cc09ba
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-05-15 09:17:36 +0900

    アップデート時にbegin_atが設定されるようにして、nullでもrangeは問題なくする

--- app/models/ipv6_neighbor.rb
+++ app/models/ipv6_neighbor.rb
@@ -8,11 +8,20 @@ class Ipv6Neighbor < ApplicationRecord
 
   alias_attribute :discovered_at, :end_at
 
-  before_save :set_begin_at
+  before_save :set_begin_at_if_null
 
-  def set_begin_at
-    return unless begin_at.nil?
+  def set_begin_at_if_null
+    return if begin_at
 
-    self.begin_at = end_at
+    # アップデートされているときは、以前の値を優先して設定する
+    self.begin_at = end_at_was || end_at
+  end
+
+  def range
+    if begin_at
+      (begin_at..end_at)
+    else
+      (end_at..end_at)
+    end
   end
 end
