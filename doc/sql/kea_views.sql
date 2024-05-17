a6b49033a2c5e91144c411b173ba18ede54883cf
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-05-17 10:24:00 +0900

    コンマがなかった

--- doc/sql/kea_views.sql
+++ doc/sql/kea_views.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE VIEW ipv6_reservations_alt AS SELECT
   address,
   prefix_len,
   type AS reservation_type,
-  dhcp6_iaid
+  dhcp6_iaid,
   host_id FROM ipv6_reservations;
 
 CREATE OR REPLACE VIEW host_identifier_type_alt AS SELECT
