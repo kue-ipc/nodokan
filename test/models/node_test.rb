13f8254dcbddd8a67f54305ced5773f93db85422
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-05-20 12:26:03 +0900

    フラグの処理

--- test/models/node_test.rb
+++ test/models/node_test.rb
@@ -7,45 +7,61 @@ class NodeTest < ActiveSupport::TestCase
 
   test "flag" do
     @node.specific = false
-    @node.logical = false
+    @node.public = false
+    @node.dns = false
     assert_nil @node.flag
 
     @node.specific = true
-    @node.logical = false
+    @node.public = false
+    @node.dns = false
     assert_equal "s", @node.flag
 
     @node.specific = false
-    @node.logical = true
-    assert_equal "l", @node.flag
+    @node.public = true
+    @node.dns = false
+    assert_equal "p", @node.flag
+
+    @node.specific = false
+    @node.public = false
+    @node.dns = true
+    assert_equal "d", @node.flag
 
     @node.specific = true
-    @node.logical = true
-    assert_includes ["ls", "sl"], @node.flag
+    @node.public = true
+    @node.dns = true
+    assert_equal "spd".each_char.sort, @node.flag.each_char.sort
   end
 
   test "flag assign" do
-    @node.flag = "ls"
-    assert @node.specific
-    assert @node.logical
-
     @node.flag = nil
     assert_not @node.specific
-    assert_not @node.logical
+    assert_not @node.public
+    assert_not @node.dns
 
     @node.flag = ""
     assert_not @node.specific
-    assert_not @node.logical
+    assert_not @node.public
+    assert_not @node.dns
 
     @node.flag = "s"
     assert @node.specific
-    assert_not @node.logical
+    assert_not @node.public
+    assert_not @node.dns
 
-    @node.flag = "l"
+    @node.flag = "p"
     assert_not @node.specific
-    assert @node.logical
+    assert @node.public
+    assert_not @node.dns
 
-    @node.flag = "sl"
+    @node.flag = "d"
+    assert_not @node.specific
+    assert_not @node.public
+    assert @node.dns
+
+    @node.flag = "spd"
     assert @node.specific
-    assert @node.logical
+    assert @node.public
+    assert @node.dns
+
   end
 end
