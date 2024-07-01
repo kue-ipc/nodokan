94567b43ca9585bd3765ee6dbd3fa795ec1eff5a
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-07-02 07:10:29 +0900

    seedでメッセージが出るようにする

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-def create_models(model_class)
+def create_entities(model_class)
   return unless model_class.count.zero?
 
-  Rails.logger.debug do
-    "model: #{model_class.name}"
-  end
+  start_msg = "create entities start: #{model_class.name}"
+  puts start_msg
+  Rails.logger.info("db:seed") { start_msg }
+  start_time = Time.now
+  count = {success: 0, failure: 0}
 
   seeds_path = Rails.root / "db" / "seeds"
   file_name = "#{model_class.name.underscore.pluralize}.yml"
@@ -19,21 +21,32 @@ def create_models(model_class)
     aliases: true, symbolize_names: false).each do |data|
     model = model_class.new(data)
     if model.save
-      Rails.logger.debug { "succeeded to create: #{model.name}" }
+      count[:success] += 1
+      Rails.logger.debug("db:seed") {
+        "succeeded to create: #{model.name}"
+      }
     else
-      Rails.logger.debug {
-        "faild to create: #{model.name}: #{model.errors.to_json}"
+      count[:failure] += 1
+      Rails.logger.warn("db:seed") {
+        "failed to create: #{model.name}: #{model.errors.to_json}"
       }
     end
   end
+
+  end_time = Time.now
+  end_msg = "create entities end: #{model_class.name} " \
+            "[success: #{count[:success]}, failure: #{count[:failure]}] " \
+            "(#{'%.4f' % (end_time - start_time)})"
+  puts end_msg
+  Rails.logger.info("db:seed") { end_msg }
 end
 
-create_models(DeviceType)
-# create_models(Hardware)
+create_entities(DeviceType)
+# create_entities(Hardware)
 
-create_models(OsCategory)
-create_models(OperatingSystem)
-create_models(SecuritySoftware)
+create_entities(OsCategory)
+create_entities(OperatingSystem)
+create_entities(SecuritySoftware)
 
-create_models(Network) if Rails.env.development?
-create_models(Node) if Rails.env.development?
+create_entities(Network) if Rails.env.development?
+create_entities(Node) if Rails.env.development?
