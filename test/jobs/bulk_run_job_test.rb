9a0706dca71178d94fae9285e82960fceb688c75
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-08-20 16:54:49 +0900

    exportを作成

--- test/jobs/bulk_run_job_test.rb
+++ test/jobs/bulk_run_job_test.rb
@@ -20,4 +20,23 @@ class BulkRunJobTest < ActiveJob::TestCase
     assert_equal "パソコン", node.name
     assert_equal "LAN", node.nics.first.name
   end
+
+  test "run export" do
+    bulk = bulks(:export)
+    perform_enqueued_jobs do
+      BulkRunJob.perform_later(bulk)
+    end
+
+    bulk = Bulk.find(bulk.id)
+    assert_equal "succeeded", bulk.status
+    output = bulk.output.open do |data|
+      data.set_encoding("UTF-8", "UTF-8")
+      first_char = data.getc
+      assert_equal "\u{feff}", first_char
+      csv = CSV.new(data, headers: :first_row)
+      csv.read.map(&:to_hash)
+    end
+    node = Node.find(output[0]["id"])
+    assert_equal node.name, output[0]["name"]
+  end
 end
