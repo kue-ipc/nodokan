e260affa39db0098f8d903113167c765ab0fb23d
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-03-21 11:10:51 +0900

    localhost以外で、requseのジョブ状況が見えなくなってしまう問題の解決

---
+++ config/initializers/sinatra.rb
@@ -0,0 +1,12 @@
+# sinatra
+
+# TODO: issueの対応状況に応じて変更すること
+# sinatra used by resque serevr, but dose not set permitted_hosts
+# https://github.com/resque/resque/issues/1908
+
+Rails.application.config.after_initialize do
+  require "sinatra/base"
+  class Sinatra::Base
+    set :host_authorization, permitted_hosts: Rails.application.config.hosts.map(&:to_s)
+  end
+end
