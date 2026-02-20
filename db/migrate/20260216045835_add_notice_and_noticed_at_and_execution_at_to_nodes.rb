1a1e61fd3f90fa248ad9e524d33f7b4e82ed20e8
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2026-02-20 09:55:09 +0900

    noticeのnilを許可する

--- db/migrate/20260216045835_add_notice_and_noticed_at_and_execution_at_to_nodes.rb
+++ db/migrate/20260216045835_add_notice_and_noticed_at_and_execution_at_to_nodes.rb
@@ -1,6 +1,6 @@
 class AddNoticeAndNoticedAtAndExecutionAtToNodes < ActiveRecord::Migration[8.1]
   def change
-    add_column :nodes, :notice, :integer, null: false, default: 0, limit: 1
+    add_column :nodes, :notice, :integer, limit: 1
     add_column :nodes, :noticed_at, :timestamp
     add_column :nodes, :execution_at, :timestamp
   end
--- db/migrate/20260217064345_add_content_type_to_bulks.rb
+++ db/migrate/20260217064345_add_content_type_to_bulks.rb
@@ -1,6 +1,6 @@
 class AddContentTypeToBulks < ActiveRecord::Migration[8.1]
   def change
-    # set default value to avoid error when validating existing records
+    # NOTE: set default value to avoid error when validating existing records
     add_column :bulks, :content_type, :string, default: "text/csv"
     change_column_default :bulks, :content_type, from: "text/csv", to: nil
   end
