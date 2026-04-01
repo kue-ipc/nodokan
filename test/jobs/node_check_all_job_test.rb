70fa498ea5a189f446fe40f833674b7e3b8f07d4
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2026-04-01 11:04:30 +0900

    全体チェックのテスト

--- test/jobs/node_check_all_job_test.rb
+++ test/jobs/node_check_all_job_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class NodeCheckAllJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionMailer::TestHelper
+
+  test "node check" do
+    assert_enqueued_email_with NoticeNodesMailer, :unowned, params: {nodes: [nodes(:unowned_node)]} do
+      assert_enqueued_email_with NoticeNodesMailer, :deleted_owner, params: {nodes: [nodes(:deleted_user_node)]} do
+        assert_enqueued_with(job: NodeCheckPerUserJob) do
+          # use perform_now for check enqueued jobs
+          NodeCheckAllJob.perform_now
+        end
+      end
+    end
+  end
 end
