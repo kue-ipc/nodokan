c841da7fd65455f20aee8ac41b970f5c58365479
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2026-07-28 15:47:54 +0900

    v0.8.5

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module Nodokan
-  VERSION = "0.8.4"
+  VERSION = "0.8.5"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
